75150a979ed0ca69892bf24522a7757c50b67d43
Githaiga <pythongeeko@gmail.com>
2024-01-26 10:10:19 +0300

    modify redirect uri

--- oauth/views.py
+++ oauth/views.py
@@ -21,29 +21,13 @@ class GitHubAuthView(APIView):
                     'client_id': data['client_id'],
                     'client_secret': data['client_secret'],
                     'code': data['code'],
-                    'redirect_uri': settings.GITHUB_REDIRECT_URL,
+                    'redirect_uri': data['redirect_uri'],
                 },
             )
 
-            response_json = response.json()
-            if response_json.get('error') == 'redirect_uri_mismatch':
-                # If the error is 'redirect_uri_mismatch', try again with a different redirect_uri
-                response = requests.post(
-                    'https://github.com/login/oauth/access_token',
-                    headers={
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    data={
-                        'client_id': data['client_id'],
-                        'client_secret': data['client_secret'],
-                        'code': data['code'],
-                        'redirect_uri': 'http://localhost:5173/auth/github',
-                    },
-                )
-
-            # Handle the response from either the first or second request
             if response.status_code == 200:
-                return Response(response.json(), status=200)
+                return Response(response.json())
             else:
                 return Response({'details': 'Failed to retrieve access token'}, status=response.status_code)
+        else:
+            return Response(serializer.errors, status=400)
